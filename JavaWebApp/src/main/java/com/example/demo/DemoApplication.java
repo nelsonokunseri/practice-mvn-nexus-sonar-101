6ea16280b850d7ac05a4f1153ae50d1432efc259
nelson okunseri <nelsonokunseri@gmail.com>
2023-01-19 06:42:19 -0600

    corrected

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -8,10 +8,10 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		string DBPassword = "Admin007";
 		        
-		System.out.println("Hello");
-		System.out.println("World");
+		String name = "nelson";
+
+		System.out.println(name);
 	}
 
 }
